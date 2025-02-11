0602c8d97c79cfaee2a284a3ebeb1be3de56e419
maknaeq <bauduinquentinn@gmail.com>
2025-02-11 14:07:47 +0100

    feat: implement authentication with NextAuth and Google provider, add sign in/out components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import SignIn from "@/components/SignIn";
+import { auth } from "@/auth";
+import SignOut from "@/components/SignOut";
+
+export default async function Home() {
+  const session = await auth();
+  const user = session?.user;
+  console.log(user);
   return (
     <div>
       <h1>Bōken</h1>
+      {user ? <SignOut /> : <SignIn />}
     </div>
   );
 }
