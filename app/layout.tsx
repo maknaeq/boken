5ed629b3bbe5c62a745593140c461cfea918033d
maknaeq <bauduinquentinn@gmail.com>
2025-02-16 16:19:18 +0100

    feat: enhance layout with new font and menu icon, remove unused button components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
+import { Bricolage_Grotesque, Darumadrop_One } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
@@ -9,8 +9,14 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import { frFR } from "@clerk/localizations";
+import { Menu } from "lucide-react";
 
 const brocolageGrotesque = Bricolage_Grotesque({
+  subsets: ["latin"],
+});
+
+const darumadropOne = Darumadrop_One({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -28,10 +34,14 @@ export default function RootLayout({
     <ClerkProvider localization={frFR}>
       <html lang="fr">
         <body className={`${brocolageGrotesque.className}`}>
-          <div className="flex items-center justify-between">
-            <h1>Bōken</h1>
+          <div className="flex items-center justify-between p-3">
+            <Menu />
+            <div>
+              <h1>Bōken</h1>
+              <span className={darumadropOne.className}>ぼうけん</span>
+            </div>
 
-            <div className="border border-red-500">
+            <div>
               <SignedOut>
                 <SignInButton />
               </SignedOut>
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default async function Home() {
-  return (
-    <div className="space-y-4">
-      <Button variant="default" className="block">
-        Default
-      </Button>
-      <Button variant="destructive" className="block">
-        Destructive
-      </Button>
-      <Button variant="secondary" className="block">
-        Secondary
-      </Button>
-      <Button variant="ghost" className="block">
-        Ghost
-      </Button>
-      <Button variant="link" className="block">
-        Link
-      </Button>
-      <Button variant="outline" className="block">
-        Outline
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
