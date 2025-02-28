fa297fe213c949cd3c8cd0246a3df754f6e195d5
maknaeq <bauduinquentinn@gmail.com>
2025-02-28 10:59:59 +0100

    feat: update user login card to fetch user details and improve display logic

--- components/user-login-card.tsx
+++ components/user-login-card.tsx
@@ -13,75 +13,83 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { LogOut } from "lucide-react";
 import RedirectButton from "./redirect-button";
+import { getCurrentUserByEmail } from "@/app/actions/userActions";
 
 async function UserLoginCard() {
   const session = await auth();
-  const user = session?.user;
-  const name = user?.name || user?.email;
-  if (user) {
-    return (
-      <div className="flex items-center space-x-2">
-        {/* This button check the pathname and is disabled depending on protected paths */}
-        <RedirectButton variant={"outline"} href="/dashboard/trips">
-          Mes voyages
-        </RedirectButton>
+  const user = await getCurrentUserByEmail(session?.user?.email as string);
 
-        <DropdownMenu>
-          <DropdownMenuTrigger className="relative">
-            <Avatar className="ring-gray-900 ring-offset-2 transition-all ease-out hover:ring-2">
-              <AvatarImage src={user?.image as string} />
-              <AvatarFallback className="bg-yellow-300">
-                {name?.charAt(0).toUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="text-nowrap rounded-xl" align="end">
-            <div className="flex space-x-3 p-2">
-              <Avatar>
-                <AvatarImage src={user?.image as string} />
-                <AvatarFallback className="bg-yellow-300">
-                  {name?.charAt(0).toUpperCase()}
-                </AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col">
-                <DropdownMenuLabel className="p-0 font-normal">
-                  {user?.name || "First LASTNAME"}
-                </DropdownMenuLabel>
-                <DropdownMenuLabel className="text-nowrap p-0 font-normal text-gray-500">
-                  {user?.email}
-                </DropdownMenuLabel>
-              </div>
-            </div>
-            <DropdownMenuSeparator />
-            <div className="text-gray-500">
-              <form
-                action={async () => {
-                  "use server";
-                  await signOut();
-                }}
-              >
-                <DropdownMenuItem asChild>
-                  <button type="submit" className="w-full cursor-pointer">
-                    <LogOut />
-                    Déconnexion
-                  </button>
-                </DropdownMenuItem>
-              </form>
-            </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+  if (!session || !user || user.length === 0) {
+    return (
+      <>
+        <Link href="/login">
+          <Button variant="ghost">Connexion</Button>
+        </Link>
+        <Link href="/signup">
+          <Button variant="default">Inscription</Button>
+        </Link>
+      </>
     );
   }
+
+  const currentUser = user[0];
+  const displayName =
+    currentUser.name ||
+    `${currentUser.firstName} ${currentUser.lastName?.toUpperCase()}` ||
+    currentUser.email ||
+    "Inconnu";
+
   return (
-    <>
-      <Link href="/login">
-        <Button variant="ghost">Connexion</Button>
-      </Link>
-      <Link href="/signup">
-        <Button variant="default">Inscription</Button>
-      </Link>
-    </>
+    <div className="flex items-center space-x-2">
+      <RedirectButton variant={"outline"} href="/dashboard/trips">
+        Mes voyages
+      </RedirectButton>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="relative">
+          <Avatar className="ring-gray-900 ring-offset-2 transition-all ease-out hover:ring-2">
+            <AvatarImage src={currentUser.image as string} />
+            <AvatarFallback className="bg-yellow-300">
+              {displayName.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="text-nowrap rounded-xl" align="end">
+          <div className="flex space-x-3 p-2">
+            <Avatar>
+              <AvatarImage src={currentUser.image as string} />
+              <AvatarFallback className="bg-yellow-300">
+                {displayName.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col">
+              <DropdownMenuLabel className="p-0 font-normal">
+                {displayName}
+              </DropdownMenuLabel>
+              <DropdownMenuLabel className="text-nowrap p-0 font-normal text-gray-500">
+                {currentUser.email}
+              </DropdownMenuLabel>
+            </div>
+          </div>
+          <DropdownMenuSeparator />
+          <div className="text-gray-500">
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <DropdownMenuItem asChild>
+                <button type="submit" className="w-full cursor-pointer">
+                  <LogOut />
+                  Déconnexion
+                </button>
+              </DropdownMenuItem>
+            </form>
+          </div>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
 
